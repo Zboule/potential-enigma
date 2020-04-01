ebc4c3e4b0edebb88894345d41b680d0895ae269
jordane <jordane.cure@gmail.com>
2020-04-01 08:43:24 +0200

    Add prod folder

---
+++ build/precache-manifest.ee15c67817fd63b76c96e4330dba8f25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b1b5f69fd83a9d0f7e02c922dcaf4b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31969b8b0bb9853a5acf",
+    "url": "/static/css/2.3b0c3cf8.chunk.css"
+  },
+  {
+    "revision": "e10e3ae5fe82909e4694",
+    "url": "/static/css/main.076a468b.chunk.css"
+  },
+  {
+    "revision": "31969b8b0bb9853a5acf",
+    "url": "/static/js/2.9b1068ee.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.9b1068ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e10e3ae5fe82909e4694",
+    "url": "/static/js/main.db36b3d8.chunk.js"
+  },
+  {
+    "revision": "ad67909a289493d0527b",
+    "url": "/static/js/runtime-main.8402527d.js"
+  }
+]);
